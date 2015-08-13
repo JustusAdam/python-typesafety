c9896f5edc486e696220229a79589778fa1c3800
Justus Adam <dev@justus.science>
2015-08-13 21:58:57 +0200

    More efficiency in optimized interpreter mode.

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-Python typesafety
-====
+# Python typesafety
 
 Python typesafety provides a decorator for python functions which uses the normally optional type annotations and performs typechecking based on these annotations both on the function arguments (before function execution) and the return (after function execution).
 
 Non annotated values will not be checked.
 
 Since annotations were only added in python3 this decorator will not work for previous versions of python. Very unfortunate, annotations are awesome. :)
+
+# Overhead
+
+Despite my best attempts at making this decorator as fast and efficient as possible it still introduces a substantial amount of overhead into your program.
+
+However it is designed such that if the python interpreter has been invoked in optimized mode (-O option or PYTHONOPTIMIZE environment variable) the decorator will just return the function itself, posing zero runtime overhead.
+
+Ergo as long as your project runs in optimized mode on production you don't have to delete the decorator in production.
--- typesafe/_decorator.py
+++ typesafe/_decorator.py
@@ -1,17 +1,41 @@
 import inspect
-from functools import partial
+import string
 
 __author__ = 'justusadam'
 
 
+ERROR_MESSAGE = string.Template(
+    'Expected type \'$expected\' for argument \'$name\', but got \'$received\'.'
+)
+
+
 def typesafe(func):
+    """
+    Tests the decorated function on every call to see if the arguments and
+    return values line up with any potential type hints given in the function
+    signature.
+
+    This decorator will only check for correctness of type if the interpreter
+    is run in debug mode (default method of invocation).  If the interpreter was
+    invoked invoked in optimised mode (-O option or PYTHONOPTIMIZE specified)
+    this decorator will just return the function unchanged, thus creating zero
+    runtime overhead.
+    """
+
     spec = inspect.getfullargspec(func)
     types = spec.annotations
     def_args = spec.args
 
-    def checkargs(argval):
-        for arg, value in argval:
-            assert isinstance(value, types.get(arg, object))
+    def checkargs(argval_pairs):
+        for arg, value in argval_pairs:
+            if not isinstance(value, types.get(arg, object)):
+                raise TypeError(
+                    ERROR_MESSAGE.substitute(
+                        expected=types[arg],
+                        received=type(value),
+                        name=arg
+                    )
+                )
 
     def wrap(*args, **kwargs):
 
@@ -21,8 +45,11 @@ def typesafe(func):
         checkargs(kwargs.items())
 
         res = func(*args, **kwargs)
-        
+
         checkargs([('return', res)])
         return res
 
-    return wrap
+    if __debug__:
+        return wrap
+    else:
+        return func
