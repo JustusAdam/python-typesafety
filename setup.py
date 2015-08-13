1df9395067a692577260027e70eac03045200539
Justus Adam <dev@justus.science>
2015-08-13 22:09:59 +0200

    added setup.py
    I want to upload this to PyPi

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+
+setup(name='typesafety',
+    version='1.0',
+    description='Efficient general typesafety based on type hints.',
+    author='Justus Adam',
+    author_email='dev@justus.science',
+    maintainer_email='dev@justus.science',
+    platforms=['POSIX', 'Windows'],
+    license='GPLv2',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: GPL version 2' 'development',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+    long_description='This package provides a simple decorator that will '
+    'check the type of supplied arguments and return values of decorated '
+    'functions on every call.',
+    url='https://github.com/JustusAdam/python-typesafety',
+    packages=['typesafe'],
+    )
